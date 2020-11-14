e21a33568859cd1bcf7a775a9d80c146c0c4a547
Charlie <13856015+KyrneDev@users.noreply.github.com>
2020-11-14 11:57:09 -0800

    AdminUX

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,8 +1,27 @@
-import { extend } from 'flarum/extend';
 import app from 'flarum/app';
 
-import PusherSettingsModal from './components/PusherSettingsModal';
-
 app.initializers.add('flarum-pusher', app => {
-  app.extensionSettings['flarum-pusher'] = () => app.modal.show(PusherSettingsModal);
+  app.extensionData.for('flarum-pusher')
+    .registerSettings([
+      {
+        setting: 'flarum-pusher.app_id',
+        label: app.translator.trans('flarum-pusher.admin.pusher_settings.app_id_label'),
+        type: 'text'
+      },
+      {
+        setting: 'flarum-pusher.app_key',
+        label: app.translator.trans('flarum-pusher.admin.pusher_settings.app_key_label'),
+        type: 'text'
+      },
+      {
+        setting: 'flarum-pusher.app_secret',
+        label: app.translator.trans('flarum-pusher.admin.pusher_settings.app_secret_label'),
+        type: 'text'
+      },
+      {
+        setting: 'flarum-pusher.app_cluster',
+        label: app.translator.trans('flarum-pusher.admin.pusher_settings.app_cluster_label'),
+        type: 'text'
+      }
+    ])
 });
